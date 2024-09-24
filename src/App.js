c0f1adb0a92f41e7b90b0c0ac9ede1b6c2c3192f
blckdfly <naufvll.xx@gmail.com>
2024-09-24 10:30:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p> 
+            We're on working on something exciting, Stay tune for update!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
   );
